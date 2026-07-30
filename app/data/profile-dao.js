601191d9270fc6b3ced163497594151860cb645a
murdahlob <alexey.bachihin@gmail.com>
2026-07-30 22:05:56 +0300

    build profile update document from a field list

--- app/data/profile-dao.js
+++ app/data/profile-dao.js
@@ -12,81 +12,54 @@ function ProfileDAO(db) {
 
     const users = db.collection("users");
 
-    /* Fix for A6 - Sensitive Data Exposure
+    const PROFILE_FIELDS = [
+        "firstName",
+        "lastName",
+        "address",
+        "bankAcc",
+        "bankRouting",
+        "ssn",
+        "dob"
+    ];
 
-    // Use crypto module to save sensitive data such as ssn, dob in encrypted format
-    const crypto = require("crypto");
-    const config = require("../../config/config");
+    /**
+     * Copy the submitted profile values into an update document. Fields the
+     * form left empty are skipped so they are not overwritten with blanks.
+     */
+    const buildUpdateDocument = (submitted) => {
+        const user = {};
 
-    /// Helper method create initialization vector
-    // By default the initialization vector is not secure enough, so we create our own
-    const createIV = () => {
-        // create a random salt for the PBKDF2 function - 16 bytes is the minimum length according to NIST
-        const salt = crypto.randomBytes(16);
-        return crypto.pbkdf2Sync(config.cryptoKey, salt, 100000, 512, "sha512");
+        PROFILE_FIELDS.forEach((field) => {
+            if (submitted[field]) {
+                user[field] = submitted[field];
+            }
+        });
+
+        return user;
     };
-
-    // Helper methods to encryt / decrypt
-    const encrypt = (toEncrypt) => {
-        config.iv = createIV();
-        const cipher = crypto.createCipheriv(config.cryptoAlgo, config.cryptoKey, config.iv);
-        return `${cipher.update(toEncrypt, "utf8", "hex")} ${cipher.final("hex")}`;
-    };
-
-    const decrypt = (toDecrypt) => {
-        const decipher = crypto.createDecipheriv(config.cryptoAlgo, config.cryptoKey, config.iv);
-        return `${decipher.update(toDecrypt, "hex", "utf8")} ${decipher.final("utf8")}`;
-    };
-    */
 
     this.updateUser = (userId, firstName, lastName, ssn, dob, address, bankAcc, bankRouting, callback) => {
 
-        // Create user document
-        const user = {};
-        if (firstName) {
-            user.firstName = firstName;
-        }
-        if (lastName) {
-            user.lastName = lastName;
-        }
-        if (address) {
-            user.address = address;
-        }
-        if (bankAcc) {
-            user.bankAcc = bankAcc;
-        }
-        if (bankRouting) {
-            user.bankRouting = bankRouting;
-        }
-        if (ssn) {
-            user.ssn = ssn;
-        }
-        if (dob) {
-            user.dob = dob;
-        }
-        /*
-        // Fix for A7 - Sensitive Data Exposure
-        // Store encrypted ssn and DOB
-        if(ssn) {
-            user.ssn = encrypt(ssn);
-        }
-        if(dob) {
-            user.dob = encrypt(dob);
-        }
-        */
+        const user = buildUpdateDocument({
+            firstName,
+            lastName,
+            address,
+            bankAcc,
+            bankRouting,
+            ssn,
+            dob
+        });
 
         users.update({
                 _id: parseInt(userId)
             }, {
                 $set: user
             },
-            err => {
-                if (!err) {
-                    console.log("Updated user profile");
-                    return callback(null, user);
-                }
+            (err) => {
+                if (err) return callback(err, null);
 
-                return callback(err, null);
+                console.log("Updated user profile");
+                return callback(null, user);
             }
         );
     };
@@ -95,17 +68,7 @@ function ProfileDAO(db) {
         users.findOne({
                 _id: parseInt(userId)
             },
-            (err, user) => {
-                if (err) return callback(err, null);
-                /*
-                // Fix for A6 - Sensitive Data Exposure
-                // Decrypt ssn and DOB values to display to user
-                user.ssn = user.ssn ? decrypt(user.ssn) : "";
-                user.dob = user.dob ? decrypt(user.dob) : "";
-                */
-
-                callback(null, user);
-            }
+            (err, user) => err ? callback(err, null) : callback(null, user)
         );
     };
 }
